13cdf18d7a51e303ae23ff39fdab0c4243c606d5
Aline Serafim <aline.serafim86@gmail.comgit config --global user.name Aline>
2024-03-27 16:02:29 -0300

    Estilização do navbar e footer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
 import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects';
 
 import Container from './components/layout/Container';
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <BrowserRouter>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/company'>Empresa</Link></li>
-        <li><Link to='/contact'>Contato</Link></li>
-        <li><Link to='/newproject'>Novo Projeto</Link></li>
-      </ul>
-
+      <Navbar/>
       <Container customClass='min-height'>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
           <Route path='/company' element={<Company />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/newproject' element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer/>
     </BrowserRouter>
   );
 }
