e34c0852930611b34924cc1e890a8e5ac1e3cefe
Aline Serafim <aline.serafim86@gmail.comgit config --global user.name Aline>
2024-04-14 18:26:42 -0300

    Exibindo detalhes do projeto

--- src/components/layout/Loading.js
+++ src/components/layout/Loading.js
@@ -1,11 +1,11 @@
-import moneyBag from '../../img/money-bag.png'
+import loading from '../../img/loading.jpg'
 
 import styles from './Loading.module.css'
 
 function Loading() {
     return (
         <div className={styles.loader_container}>
-            <img className={styles.loader} src={moneyBag} alt='Loading'/>
+            <img className={styles.loader} src={loading} alt='Loading'/>
         </div>
     )
 }
